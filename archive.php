6f4d132209f115db51e0afdd819a8cfd94bb77fb
Kiran Shahi <mail@kirans.com.np>
2016-07-14 21:09:12 +0545

    Archive has been added to theme.

---
+++ archive.php
@@ -0,0 +1,149 @@
+<?php  
+    get_header();
+?>
+<header class="intro-header" style="background-image: url('http://www.kirans.com.np/wp-content/themes/kirans/nepal.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <div class="site-heading">
+                    <h1>Howdy!</h1>
+                    <hr class="small">
+                    <span class="subheading"><?php bloginfo('description'); ?></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<div class="container">
+<div class="row">
+<div class="col-lg-8">
+
+<?php
+    if (have_posts()) :
+ ?>
+    	<h3><?php 
+
+	if ( is_category() ) {
+		single_cat_title();
+	} elseif ( is_tag() ) {
+		single_tag_title();
+	} elseif ( is_author() ){
+		the_post();
+		echo 'Author Archives: ' . get_the_author();
+		rewind_posts();	
+	} elseif ( is_day() ) {
+		echo 'Daily Archives: ' . get_the_date();
+	} elseif ( is_month() ) {
+		echo 'Monthly Archives: ' . get_the_date('F Y');
+	} elseif ( is_year() ) {
+		echo 'Yearly Archives: ' . get_the_date('Y');
+	} else {
+		echo "Archives:";
+	}
+
+	 ?></h3>
+	 <?php
+
+        while (have_posts()) : the_post(); 
+    ?>
+    
+    <div class="post-preview">
+        <a href="<?php the_permalink(); ?>">
+            <h2 class="post-title">
+                <?php the_title(); ?>
+            </h2>
+            <p class="post-subtitle">
+                <?php echo get_the_excerpt(); ?>
+            </p>
+        </a>
+
+        <p class="post-meta">
+            Published by 
+            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+                <?php the_author(); ?>
+            </a> 
+            on <?php the_time('F j, Y'); ?> <i class="fa fa-tags" aria-hidden="true"></i> 
+            <?php 
+                $categories = get_the_category();
+                $separator = ", ";
+                $output = '';
+
+                if ($categories) {
+                    foreach ($categories as $category)
+                        $output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>' . $separator;
+                } 
+
+                echo trim($output, $separator);
+
+            ?>
+        </p>
+    </div>
+    
+    <hr>
+
+    <?php  endwhile;
+        else :
+            echo '<h1>Opps! No content found.</h1>';
+        endif;
+
+        echo    "</div>";
+
+
+        ?>
+
+        <!-- Blog Sidebar Widgets Column -->
+        <div class="col-md-4">
+        
+                      <?php get_search_form(); ?>
+
+                <!-- Blog Categories Well -->
+                <div class="well">
+                    <h4>Blog Categories</h4>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <ul class="list-unstyled">
+                                <li><a href="#">Category Name</a>
+                                </li>
+                                <li><a href="#">Category Name</a>
+                                </li>
+                                <li><a href="#">Category Name</a>
+                                </li>
+                                <li><a href="#">Category Name</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6">
+                            <ul class="list-unstyled">
+                                <li><a href="#">Category Name</a>
+                                </li>
+                                <li><a href="#">Category Name</a>
+                                </li>
+                                <li><a href="#">Category Name</a>
+                                </li>
+                                <li><a href="#">Category Name</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                </div>
+
+                <!-- Side Widget Well -->
+                <div class="well">
+                    <h4>Side Widget Well</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+                </div>
+
+            </div>
+
+        </div>
+        <!-- /.row -->
+        <hr>
+       </div>
+       </div>
+       </div>
+<?php
+    get_footer();
+
+?>
--- page-about.php
+++ page-about.php
@@ -4,7 +4,7 @@
 
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url('../wp-content/themes/mero/resources/img/about-bg.jpg')">
+    <header class="intro-header" style="background-image: url('../wp-content/themes/kirans/resources/img/about-bg.jpg')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
